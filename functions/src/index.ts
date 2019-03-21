8520656729086c9df558a46ed5153bf043bd7ea6
yashkhade <ykhade@nevada.unr.edu>
2019-03-21 14:47:35 -0700

    Exported the functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,2 +1,6 @@
+//All the cloud functions are exported to the index for invocation.
+
 export {basicHTTP, api} from './http';
 export {createUserRecord} from './auth';
+export {gameCount, userTrend} from './firestore';
+export {resizeAvatar} from './storage';
