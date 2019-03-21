66c5eed3bd3be4b6b9a817d6062963779880b8c9
yashkhade <ykhade@nevada.unr.edu>
2019-03-21 14:01:40 -0700

    Created auth cloud function

---
+++ functions/src/auth.ts
@@ -0,0 +1,19 @@
+import * as functions from 'firebase-functions';
+
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const createUserRecord = functions.auth
+    .user()
+    .onCreate((user, context) => {
+        const userRef = db.doc(`users/${user.uid}`);
+
+        return userRef.set({
+            name: user.displayName,
+            createAt: context.timestamp,
+            nickname: 'buddy'
+        });
+    });
